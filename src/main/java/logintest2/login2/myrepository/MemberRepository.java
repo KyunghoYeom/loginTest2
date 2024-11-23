a12f5842b769520c65cf41be1566104366f39482
KyunghoYeom <hoyeom1269@naver.com>
2024-11-23 11:21:36 +0900

    memberRepository save 기능 구현

--- src/main/java/logintest2/login2/myrepository/MemberRepository.java
+++ src/main/java/logintest2/login2/myrepository/MemberRepository.java
@@ -1,13 +1,19 @@
 package logintest2.login2.myrepository;
 
 import jakarta.persistence.EntityManager;
+import logintest2.login2.domain.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
 public class MemberRepository {
+    EntityManager em;
 
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
 
 
 
